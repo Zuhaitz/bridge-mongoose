364eb6b5729ae15288739a8bc45f3c7025662312
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-07-03 20:37:04 +0200

    added indexes to values

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema({
-  name: String,
-  price: Number,
+  name: { type: String, unique: true },
+  price: { type: Number, required: true },
 });
 
 const Product = mongoose.model("Product", ProductSchema);
