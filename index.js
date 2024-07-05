1735027454915ff7f404f75ec43e41d1129477bd
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-07-05 20:34:18 +0200

    port added to env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { dbConnection } = require("./config/config");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT;
 
 const runServer = async () => {
   await dbConnection();
